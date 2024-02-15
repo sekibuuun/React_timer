42c917c1ca9be75d51d064a2a4ee7930d6fd7d5d
sekibuuun <akaunto.no.001@gmail.com>
2024-02-15 17:58:49 +0900

    wip: 分の対応

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -3,13 +3,19 @@ import React, { useEffect, useState } from 'react';
 function Timer () {
   const [isStarted, setIsStarted] = useState<boolean>(false)
   const [now, setNow] = useState<Date>(new Date())
-  const [duration, setDuration] = useState<number>(60)
+  const [duration, setDuration] = useState<number>(1)
 
   useEffect(() => {
     if (isStarted) {
       setInterval(() => {
-        setDuration(duration - Math.floor((new Date().getTime() - now.getTime()) / 1000))
-      },1000)
+        if (Math.floor((new Date().getTime() - now.getTime()) / (1000)) === 60) {
+          setDuration(duration - Math.floor((new Date().getTime() - now.getTime()) / (1000)))
+          setNow(new Date())
+          console.log("now3", now);
+        }
+        console.log(Math.floor((new Date().getTime() - now.getTime()) / (1000)));
+        console.log("now2", now);
+      }, 1000)
       console.log('Timer component mounted')
     }
   }, [isStarted])
@@ -17,6 +23,8 @@ function Timer () {
   const startTimer = () => {
     setIsStarted(true)
     setNow(new Date())
+    console.log("now1", now);
+    
   }
   
   if (duration <= 0) {
