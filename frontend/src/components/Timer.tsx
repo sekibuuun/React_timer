d16344aaa9636d43b7d2b8699c638d9e6b61f04e
sekibuuun <akaunto.no.001@gmail.com>
2024-02-16 17:00:11 +0900

    feat: 1秒ごとのカウントダウン

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -3,52 +3,35 @@ import React, { useEffect, useState } from 'react';
 function Timer () {
   const [isStarted, setIsStarted] = useState<boolean>(false)
   const [now, setNow] = useState<Date>(new Date())
-  const [duration, setDuration] = useState<number>(1)
+  const [duration, setDuration] = useState<number>(60)
 
   useEffect(() => {
     if (isStarted) {
       setInterval(() => {
-        if (Math.floor((new Date().getTime() - now.getTime()) / (1000)) === 60) {
-          setDuration(duration - Math.floor((new Date().getTime() - now.getTime()) / (1000)))
-          setNow(new Date())
-          console.log("now3", now);
-        }
-        console.log(Math.floor((new Date().getTime() - now.getTime()) / (1000)));
-        console.log("now2", now);
+        setDuration(duration - Math.floor((new Date().getTime() - now.getTime()) / (1000)))
       }, 1000)
-      console.log('Timer component mounted')
     }
+    console.log('Timer component mounted')
   }, [isStarted])
 
   const startTimer = () => {
     setIsStarted(true)
     setNow(new Date())
-    console.log("now1", now);
-    
   }
-  
-  if (duration <= 0) {
-    return (
-      <div>
-        <h1>Timer</h1>
-        <p>Time's up!</p>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>Timer</h1>
-        {isStarted ? 
-          null : 
-          <div>  
-            <input type="number" value={duration} onChange={(e) => setDuration(parseInt(e.target.value))}/>
-            <button onClick={() => startTimer()}>Start</button>
-          </div>
-        }
-        <p>{duration}</p>
-      </div>
-    )
-  }
+
+  return (
+    <div>
+      <h1>Timer</h1>
+      {isStarted ? 
+        null : 
+        <div>  
+          <input type="number" value={duration} onChange={(e) => setDuration(parseInt(e.target.value))}/>
+          <button onClick={() => startTimer()}>Start</button>
+        </div>
+      }
+      <p>{duration}</p>
+    </div>
+  )
 }
 
 export default Timer
