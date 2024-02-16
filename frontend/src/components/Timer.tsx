0878a9afe08d0fd9033552f1b66b2ce08b203430
sekibuuun <akaunto.no.001@gmail.com>
2024-02-16 17:35:46 +0900

    fix: 変数名の変更

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -3,12 +3,12 @@ import React, { useEffect, useState } from 'react';
 function Timer () {
   const [isStarted, setIsStarted] = useState<boolean>(false)
   const [now, setNow] = useState<Date>(new Date())
-  const [duration, setDuration] = useState<number>(60)
+  const [seconds, setSeconds] = useState<number>(60)
 
   useEffect(() => {
     if (isStarted) {
       setInterval(() => {
-        setDuration(duration - Math.floor((new Date().getTime() - now.getTime()) / (1000)))
+        setSeconds(seconds - Math.floor((new Date().getTime() - now.getTime()) / (1000)))
       }, 1000)
     }
     console.log('Timer component mounted')
@@ -25,11 +25,11 @@ function Timer () {
       {isStarted ? 
         null : 
         <div>  
-          <input type="number" value={duration} onChange={(e) => setDuration(parseInt(e.target.value))}/>
+          <input type="number" value={seconds} onChange={(e) => setSeconds(parseInt(e.target.value))}/>
           <button onClick={() => startTimer()}>Start</button>
         </div>
       }
-      <p>{duration}</p>
+      <p>{seconds}</p>
     </div>
   )
 }
