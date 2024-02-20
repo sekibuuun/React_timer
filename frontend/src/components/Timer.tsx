88daef617cf1f41cc98f397ed214774d348e46b1
sekibuuun <akaunto.no.001@gmail.com>
2024-02-20 15:40:03 +0900

    fix: 変数名の変更

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from 'react';
 
 function Timer () {
   const [isStarted, setIsStarted] = useState<boolean>(false)
-  const [now, setNow] = useState<Date>(new Date())
+  const [startTime, setStartTime] = useState<Date>(new Date())
   const [seconds, setSeconds] = useState<number>(0)
 
   useEffect(() => {
     if (isStarted) {
       setInterval(() => {
-        const count = seconds - Math.floor((new Date().getTime() - now.getTime()) / (1000))
-        console.log(count);
-        setSeconds(count)
+        const newSeconds = seconds - Math.floor((new Date().getTime() - startTime.getTime()) / (1000))
+        console.log(newSeconds);
+        setSeconds(newSeconds)
       }, 1000)
     }
     console.log('Timer component mounted')
   }, [isStarted])
 
-  const startTimer = () => {
+  const start = () => {
     setIsStarted(true)
-    setNow(new Date())
+    setStartTime(new Date())
   }
 
   return (
@@ -28,7 +28,7 @@ function Timer () {
         null : 
         <div>  
           <input type="number" value={seconds} onChange={(e) => setSeconds(parseInt(e.target.value))}/>
-          <button onClick={() => startTimer()}>Start</button>
+          <button onClick={() => start()}>Start</button>
         </div>
       }
       <p>{seconds <= 0 ? "COMPLETE" : `${Math.floor(seconds / 60).toString().padStart(2, '0')}:${(seconds % 60).toString().padStart(2, '0')}`}</p>
